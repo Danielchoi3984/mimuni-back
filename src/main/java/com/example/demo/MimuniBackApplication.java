365fcd2b258a31f8757a89834cf9570c0054b8ff
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-06-26 14:47:37 -0300

    aca agreggue el comand line runner

--- src/main/java/com/example/demo/MimuniBackApplication.java
+++ src/main/java/com/example/demo/MimuniBackApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +12,7 @@ import com.example.demo.service.ServicioProfesionalService;
 import com.example.demo.service.VecinoService;
 
 @SpringBootApplication
-public class MimuniBackApplication {
+public class MimuniBackApplication implements CommandLineRunner {
 
 	@Autowired
 	BarrioService barrioService;
@@ -30,6 +31,13 @@ public class MimuniBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MimuniBackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+//		Aca Hagamos las pruebas amigos
+		
 	}
 
 }
